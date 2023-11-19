85adda6b283bd57e41809f6556983cf4731fce28
Hello-AndyJoe <andrew.joseph.roper@gmail.com>
2023-11-18 21:06:29 -0700

    Creating basic file structure

--- server/utils/auth-bckup.js
+++ server/utils/auth-bckup.js
@@ -1,4 +1,3 @@
-const {GraphQLError} = require('graphql');
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
@@ -6,11 +5,6 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  AuthenticationError: new GraphQLError('Could not authenicate user.', {
-    extensions: {
-      code: 'UNAUTHENTICATED',
-    }
-  }),
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
