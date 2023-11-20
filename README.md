881c0e555610f4be68b87709908c130b27fda25b
Hello-AndyJoe <andrew.joseph.roper@gmail.com>
2023-11-20 16:42:05 -0700

    Changes.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Challenge-21-Book-Search-MERN
+
+https://www.apollographql.com/tutorials/fullstack-quickstart/01-introduction
+
+https://react.dev/learn
+
+https://graphql.org/graphql-js/mutations-and-input-types/
+
+Resolvers (getSingleUser, createUser, login) in server folder based on the resolvers from activity 25. Wish I understood how it worked.
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,17 +5,33 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        getSingleUser: async (parent, {userId}) => {
-            return User.findOne({_id:userId});
+        getSingleUser: async (parent, {userid}) => {
+            return User.findOne({_id:userid});
         },
     },
 
     Mutation: {
-        createUser: async (parent, {}) => {
-            return User.create({});
+        createUser: async (parent, {username, email, password}) => {
+            const newUser = await User.create({username, email, password});
+            const token = signToken(newUser);
+
+            return { token, newUser };
         },
-        login: async (parent, {}) => {
-            return User.create({});
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+        
+            if (!user) {
+                throw new AuthenticationError('Cannot find this user');
+            }
+        
+            const userPassword = await user.isCorrectPassword(password);
+        
+            if (!userPassword) {
+                throw new AuthenticationError('Wrong password!');
+            }
+        
+            const token = signToken(profile);
+            return { token, profile };
         },
         saveBook: async (parent, {}) => {
             return User.create({});
