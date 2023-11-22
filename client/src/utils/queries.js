c71a01897b78d42c8a1bd87418353cd486810238
Hello-AndyJoe <andrew.joseph.roper@gmail.com>
2023-11-21 19:14:29 -0700

    More attempts at getting things figured out.

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,19 @@
 import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
-
+    query me {
+        _id
+        email
+        password
+        savedBooks {
+          authors
+          bookId
+          description
+          image
+          link
+          title
+        }
+        username
+      }
+    }
 `;
